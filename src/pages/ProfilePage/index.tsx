e27fd46e4a0cd442f354f29ba6dc28ea42877532
Alecsander Matos <alecsander.tec@gmail.com>
2020-08-14 17:00:22 -0300

    core: Criando ProfilePage

---
+++ src/pages/ProfilePage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+
+function ProfilePage(){
+    return (
+        <View style={styles.container}>
+             <Text>Perfil Usuario</Text>
+        </View>
+    );
+}
+export default ProfilePage;
