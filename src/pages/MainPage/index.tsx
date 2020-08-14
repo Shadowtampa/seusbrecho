e4ca01034046f12b0f32856120df8878971692a5
Alecsander Matos <alecsander.tec@gmail.com>
2020-08-14 16:59:31 -0300

    core: Criando MainPage

---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+
+function MainPage(){
+    return (
+        <View style={styles.container}>
+             <Text>Pagina Inicial</Text>
+        </View>
+    );
+}
+export default MainPage;
