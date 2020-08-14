a5a24d8355b4b465b5318fa8bc996e79421f56fe
Alecsander Matos <alecsander.tec@gmail.com>
2020-08-14 17:20:14 -0300

    fix: estanciar Appstack no app

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Landing from './src/pages/MainPage';
-import LoginPage from './src/pages/LoginPage';
-
+import AppStack from "./src/routes/AppStack";
 
 export default function App() {
   return (
     <>
-      <LoginPage/>
-      <StatusBar style="auto" />
+      <AppStack/>
+      <StatusBar style="light" />
     </>
   );
 }  
