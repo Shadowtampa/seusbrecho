1862b8f5d38cce675ea4c6bd4088609ba5c690d4
Alecsander Matos <alecsander.tec@gmail.com>
2020-08-14 16:59:54 -0300

    core: Criando MarkedPAge

---
+++ src/pages/MarkedsPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+
+function MarkedsPage(){
+    return (
+        <View style={styles.container}>
+            <Text>Itens Marcados</Text>
+        </View>
+    );
+}
+export default MarkedsPage;
