2c93141629fdb024d0bbb848c2879c7d51839ae6
Alecsander Matos <alecsander.tec@gmail.com>
2020-08-14 17:00:42 -0300

    core: Criando SearchPage

---
+++ src/pages/SearchPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+
+function SearchPage(){
+    return(
+        <View style={styles.container}>
+                 <Text>Pagina de Busca</Text>
+        </View>
+    )
+}
+export default SearchPage;
